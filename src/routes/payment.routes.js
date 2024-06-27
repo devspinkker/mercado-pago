872dfb0dae7b188e1480549d0d0297e9b8494f04
Bruno Pereyra <brunopereyra459@gmail.com>
2024-06-27 18:30:42 -0300

    chat CreateChatOrGetChats

--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { createOrder, receiveWebhook } from "../controllers/payment.controller.js";
+import { auth } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.post("/create-order", authauth, createOrder);
+router.post("/create-order", auth, createOrder);
 router.post("/webhook", receiveWebhook);
 router.get("/success", (req, res) => res.send("Success"));
 
